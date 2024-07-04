ead060da984a43381c92d65d238fbd4bdfc44bd0
copilot-zd <zengdong@anta.com>
2024-07-04 13:23:58 +0800

    test commit print 2323

--- src/main/java/com/aws/codereivew/demo/CodeReviewMain.java
+++ src/main/java/com/aws/codereivew/demo/CodeReviewMain.java
@@ -5,5 +5,6 @@ public class CodeReviewMain {
 	public static void main(String[] args) {
 		System.out.println("test code reivew");
 		System.out.println("hahaha");
+		System.out.println("233434");
 	}
 }
