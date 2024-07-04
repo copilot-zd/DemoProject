d0b689118c978c14d2433df5e45058e0a2832db2
copilot-zd <zengdong@anta.com>
2024-07-04 13:06:59 +0800

    hahah

---
+++ src/main/java/com/aws/codereivew/demo/CodeReviewMain.java
@@ -0,0 +1,9 @@
+package com.aws.codereivew.demo;
+
+public class CodeReviewMain {
+
+	public static void main(String[] args) {
+		System.out.println("test code reivew");
+		System.out.println("hahaha");
+	}
+}
